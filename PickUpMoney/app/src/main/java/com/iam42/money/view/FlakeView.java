25cc36a2d0e1169b522684b53b539e87d7676311
42 <a42@42deMacBook-Pro.local>
2014-01-27 15:22:06 +0800

    surface

--- PickUpMoney/app/src/main/java/com/iam42/money/view/FlakeView.java
+++ PickUpMoney/app/src/main/java/com/iam42/money/view/FlakeView.java
@@ -12,8 +12,11 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 
 import com.iam42.money.R;
@@ -26,23 +29,57 @@ import java.util.Iterator;
  * This class is the custom view where all of the Droidflakes are drawn. This class has
  * all of the logic for adding, subtracting, and rendering Droidflakes.
  */
-public class FlakeView extends View implements View.OnTouchListener{
+public class FlakeView extends SurfaceView implements Runnable, SurfaceHolder.Callback {
+
+    private SurfaceHolder mHolder; // 用于控制SurfaceView
+    private Thread t; // 声明一条线程
+    private volatile boolean flag; // 线程运行的标识，用于控制线程
+    private Canvas mCanvas; // 声明一张画布
 
     private final int FLAKECOUNT = 30;
-    private final int LASTTIME = 10;
 
     ArrayList<Bitmap> droids;       // The bitmap that all flakes use
     ArrayList<Flake> flakes = new ArrayList<Flake>(); // List of current flakes
     ValueAnimator animator = ValueAnimator.ofFloat(0, 1);
     long startTime, prevTime; // Used to track elapsed time for animations and fps
-    Paint textScore;    // Used for rendering score text
-    Paint textTime;    // Used for rendering time text
+
     Matrix m = new Matrix(); // Matrix used to translate/rotate each flake during rendering
-    String timeString = "";
-    String numFlakesString = "";
-    float mTouchX = 0f;
-    float mTouchY = 0f;
+
     StoppedListener mStoppedListener;
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        t = new Thread(this); // 创建一个线程对象
+        flag = true; // 把线程运行的标识设置成true
+        t.start(); // 启动线程
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        flag = false; // 把线程运行的标识设置成false
+        mHolder.removeCallback(this);
+    }
+
+    @Override
+    public void run() {
+
+        while (flag) {
+            try {
+                synchronized (mHolder) {
+                    Thread.sleep(1); // 让线程休息1毫秒
+                    Draw(); // 调用自定义画画方法
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 
     public interface StoppedListener {
         public void onStoppedEaster(int count);
@@ -54,46 +91,15 @@ public class FlakeView extends View implements View.OnTouchListener{
 
     public FlakeView(Context context, AttributeSet attrs) {
         super(context, attrs);
-
-        this.setOnTouchListener(this);
+        mHolder = getHolder(); // 获得SurfaceHolder对象
+        mHolder.addCallback(this); // 为SurfaceView添加状态监听
+        setFocusable(true); // 设置焦点
 
         initWidget();
         initFlakes(FLAKECOUNT);
 
-        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator arg0) {
-                long nowTime = System.currentTimeMillis();
-                float secs = (float) (nowTime - prevTime) / 1000f;
-                prevTime = nowTime;
-                Iterator<Flake> it = flakes.iterator();
-                while (it.hasNext()) {
-                    Flake flake = it.next();
-                    flake.y += (flake.speed * secs);
-                    if (flake.y > getHeight()) {
-                        flake.y = 0 - flake.height;
-                    }
-                    if (Math.abs(flake.x - mTouchX) < 10 && Math.abs(flake.y - mTouchY) < 10) {
-                        it.remove();
-                    }
-                    flake.rotation = flake.rotation + (flake.rotationSpeed * secs);
-                }
-                setNumFlakes(FLAKECOUNT);
-                invalidate();
-            }
-        });
-        animator.setRepeatCount(ValueAnimator.INFINITE);
-        animator.setDuration(3000);
     }
 
-
-    private void setNumFlakes(int quantity) {
-        if (flakes != null) {
-            numFlakesString = "获得元宝: " + (quantity - flakes.size());
-        } else {
-            numFlakesString = "获得元宝: " + 0;
-        }
-    }
 
     private void initWidget() {
         droids = new ArrayList<Bitmap>();
@@ -101,19 +107,12 @@ public class FlakeView extends View implements View.OnTouchListener{
         droids.add(BitmapFactory.decodeResource(getResources(), R.drawable.easter2));
         droids.add(BitmapFactory.decodeResource(getResources(), R.drawable.easter3));
         droids.add(BitmapFactory.decodeResource(getResources(), R.drawable.easter4));
-        textScore = new Paint(Paint.ANTI_ALIAS_FLAG);
-        textScore.setColor(Color.WHITE);
-        textScore.setTextSize(30);
-        textTime = new Paint(Paint.ANTI_ALIAS_FLAG);
-        textTime.setColor(Color.WHITE);
-        textTime.setTextSize(60);
     }
 
     void initFlakes(int quantity) {
         for (int i = 0; i < quantity; ++i) {
             flakes.add(Flake.createFlake(getWidth(), droids));
         }
-        setNumFlakes(quantity);
     }
 
     @Override
@@ -121,36 +120,35 @@ public class FlakeView extends View implements View.OnTouchListener{
         super.onSizeChanged(w, h, oldw, oldh);
         flakes.clear();
         initFlakes(FLAKECOUNT);
-        animator.cancel();
         startTime = System.currentTimeMillis();
         prevTime = startTime;
-        animator.start();
     }
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+    private void Draw() {
 
+        long nowTime = System.currentTimeMillis();
+        float secs = (float) (nowTime - prevTime) / 1000f;
+        prevTime = nowTime;
+        Iterator<Flake> it = flakes.iterator();
+        while (it.hasNext()) {
+            Flake flake = it.next();
+            flake.y += (flake.speed * secs);
+            if (flake.y > getHeight()) {
+                flake.y = 0 - flake.height;
+            }
+            flake.rotation = flake.rotation + (flake.rotationSpeed * secs);
+        }
+
+        mCanvas = mHolder.lockCanvas(); // 获得画布对象，开始对画布画画
+        mCanvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
         for (int i = 0; i < flakes.size(); ++i) {
             Flake flake = flakes.get(i);
             m.setTranslate(-flake.width/2, -flake.height/2);
             m.postRotate(flake.rotation);
             m.postTranslate(flake.width/2 + flake.x, flake.height/2 + flake.y);
-            canvas.drawBitmap(flake.bitmap, m, null);
+            mCanvas.drawBitmap(flake.bitmap, m, null);
         }
-
-        long nowTime = System.currentTimeMillis();
-        long deltaTime = nowTime - startTime;
-        if (deltaTime > 1000) {
-            int secs = (int) (deltaTime / 1000);
-            timeString = "时间: " + secs;
-            if (secs >= LASTTIME) {
-                mStoppedListener.onStoppedEaster(FLAKECOUNT - flakes.size());
-                pause();
-            }
-        }
-        canvas.drawText(numFlakesString, 20, getHeight() - 50, textScore);
-        canvas.drawText(timeString, getWidth() - 240, 80, textTime);
+        mHolder.unlockCanvasAndPost(mCanvas);
     }
 
     public void pause() {
@@ -159,14 +157,5 @@ public class FlakeView extends View implements View.OnTouchListener{
 
     public void resume() {
         animator.start();
-    }
-
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            mTouchX = event.getX();
-            mTouchY = event.getY();
-        }
-        return false;
     }
 }
