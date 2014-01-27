047f890d86cb4907f13a0f01181897a2aca05562
42 <a42@42deMacBook-Pro.local>
2014-01-27 15:52:09 +0800

    优化

--- PickUpMoney/app/src/main/java/com/iam42/money/view/FlakeView.java
+++ PickUpMoney/app/src/main/java/com/iam42/money/view/FlakeView.java
@@ -11,13 +11,10 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
-import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.util.AttributeSet;
-import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
 
 import com.iam42.money.R;
 import com.iam42.money.model.Flake;
@@ -25,10 +22,6 @@ import com.iam42.money.model.Flake;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-/**
- * This class is the custom view where all of the Droidflakes are drawn. This class has
- * all of the logic for adding, subtracting, and rendering Droidflakes.
- */
 public class FlakeView extends SurfaceView implements Runnable, SurfaceHolder.Callback {
 
     private SurfaceHolder mHolder; // 用于控制SurfaceView
@@ -36,12 +29,14 @@ public class FlakeView extends SurfaceView implements Runnable, SurfaceHolder.Ca
     private volatile boolean flag; // 线程运行的标识，用于控制线程
     private Canvas mCanvas; // 声明一张画布
 
-    private final int FLAKECOUNT = 30;
+    private final int INITFLAKECOUNT = 30;
+    private final int ADDFLAKECOUNT = 5;
+    private final int PERADDTIME = 1;
 
     ArrayList<Bitmap> droids;       // The bitmap that all flakes use
     ArrayList<Flake> flakes = new ArrayList<Flake>(); // List of current flakes
     ValueAnimator animator = ValueAnimator.ofFloat(0, 1);
-    long startTime, prevTime; // Used to track elapsed time for animations and fps
+    long startTime, prevTime, addFlakeTime;
 
     Matrix m = new Matrix(); // Matrix used to translate/rotate each flake during rendering
 
@@ -67,26 +62,9 @@ public class FlakeView extends SurfaceView implements Runnable, SurfaceHolder.Ca
 
     @Override
     public void run() {
-
         while (flag) {
-            try {
-                synchronized (mHolder) {
-                    Thread.sleep(1); // 让线程休息1毫秒
-                    Draw(); // 调用自定义画画方法
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            Draw(); // 调用自定义画画方法
         }
-
-    }
-
-    public interface StoppedListener {
-        public void onStoppedEaster(int count);
-    }
-
-    public void setFlakeStoppedListener(StoppedListener stoppedListener) {
-        mStoppedListener = stoppedListener;
     }
 
     public FlakeView(Context context, AttributeSet attrs) {
@@ -96,7 +74,7 @@ public class FlakeView extends SurfaceView implements Runnable, SurfaceHolder.Ca
         setFocusable(true); // 设置焦点
 
         initWidget();
-        initFlakes(FLAKECOUNT);
+        addFlakes(INITFLAKECOUNT);
 
     }
 
@@ -109,36 +87,44 @@ public class FlakeView extends SurfaceView implements Runnable, SurfaceHolder.Ca
         droids.add(BitmapFactory.decodeResource(getResources(), R.drawable.easter4));
     }
 
-    void initFlakes(int quantity) {
-        for (int i = 0; i < quantity; ++i) {
-            flakes.add(Flake.createFlake(getWidth(), droids));
-        }
-    }
-
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
         flakes.clear();
-        initFlakes(FLAKECOUNT);
+        addFlakes(INITFLAKECOUNT);
         startTime = System.currentTimeMillis();
         prevTime = startTime;
+        addFlakeTime = startTime;
     }
 
     private void Draw() {
 
         long nowTime = System.currentTimeMillis();
         float secs = (float) (nowTime - prevTime) / 1000f;
+        adjustFlakeStatus(secs);
         prevTime = nowTime;
+
+        if ((nowTime - addFlakeTime) / 1000f > PERADDTIME) {
+            addFlakes(ADDFLAKECOUNT);
+            addFlakeTime = nowTime;
+        }
+
+        drawOnCanvas(flakes);
+    }
+
+    private void adjustFlakeStatus(float timeInterval) {
         Iterator<Flake> it = flakes.iterator();
         while (it.hasNext()) {
             Flake flake = it.next();
-            flake.y += (flake.speed * secs);
+            flake.y += (flake.speed * timeInterval);
             if (flake.y > getHeight()) {
-                flake.y = 0 - flake.height;
+                it.remove();
             }
-            flake.rotation = flake.rotation + (flake.rotationSpeed * secs);
+            flake.rotation = flake.rotation + (flake.rotationSpeed * timeInterval);
         }
+    }
 
+    private void drawOnCanvas(ArrayList<Flake> flakes) {
         mCanvas = mHolder.lockCanvas(); // 获得画布对象，开始对画布画画
         mCanvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
         for (int i = 0; i < flakes.size(); ++i) {
@@ -148,7 +134,16 @@ public class FlakeView extends SurfaceView implements Runnable, SurfaceHolder.Ca
             m.postTranslate(flake.width/2 + flake.x, flake.height/2 + flake.y);
             mCanvas.drawBitmap(flake.bitmap, m, null);
         }
-        mHolder.unlockCanvasAndPost(mCanvas);
+        mHolder.unlockCanvasAndPost(mCanvas); //画完显示
+    }
+
+    private void addFlakes(int count) {
+        if (flakes == null) {
+            return;
+        }
+        for (int i = 0; i < count; i++){
+            flakes.add(Flake.createFlake(getWidth(), droids));
+        }
     }
 
     public void pause() {
@@ -157,5 +152,13 @@ public class FlakeView extends SurfaceView implements Runnable, SurfaceHolder.Ca
 
     public void resume() {
         animator.start();
+    }
+
+    public interface StoppedListener {
+        public void onStoppedEaster(int count);
+    }
+
+    public void setFlakeStoppedListener(StoppedListener stoppedListener) {
+        mStoppedListener = stoppedListener;
     }
 }
